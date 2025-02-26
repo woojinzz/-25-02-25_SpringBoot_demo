1ea966ba418bf14fab9eabc087bf846528ecbc31
woojinzz <dnwls3984@gmail.com>
2025-02-26 15:53:30 +0900

    25-02-26 delete, modify, detail 추가 MVC 구조 추가 진행중 Service

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -1,41 +1,75 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.demo.service.ArticleService;
 import com.example.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
-	
-	private int lastArticleId;
-	private List<Article> articles;	
-	
-	public UsrArticleController() {
-		this.lastArticleId = 0;
-		this.articles = new ArrayList<>();
+	private ArticleService articleService;
+
+	public UsrArticleController(ArticleService articleService) {
+		this.articleService = articleService;
 	}
-	
-	@GetMapping("/usr/article/do-write")
+
+	@GetMapping("/usr/article/doWrite")
 	@ResponseBody
-	public Article doWrite( String title, String body) {
-		
-		Article article = new Article(++lastArticleId, title, body);
-		this.articles.add(article);
-		return article;
-		 
+	public Article doWrite(String title, String body) {
+		return articleService.writeArticle(title, body);
 	}
-	
-	@GetMapping("/usr/article/show-list")
+
+	@GetMapping("/usr/article/showList")
 	@ResponseBody
 	public List<Article> showList() {
-				
-		return this.articles;
-		
+		return articleService.articles;
 	}
+
+	@GetMapping("/usr/article/showDetail")
+	@ResponseBody
+	public Object showDetail(int id, String title, String body) {
+
+		Article foundArticle = articleService.getArticleById(id);
+
+		if (foundArticle == null) {
+			return id + "번 게시물은 존재하지 않습니다.";
+		}
+
+		return foundArticle;
+	}
+
+	@GetMapping("/usr/article/doMoidfy")
+	@ResponseBody
+	public String doMoidfy(int id, String title, String body) {
+
+		Article foundArticle = articleService.getArticleById(id);
+
+		if (foundArticle == null) {
+			return id + "번 게시물은 존재하지 않습니다.";
+		}
+
+		articleService.moidfyArticle(foundArticle, title, body);
+
+		return id + "번 게시물을 삭제 했습니다.";
+	}
+
+	@GetMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+
+		Article foundArticle = articleService.getArticleById(id);
+
+		if (foundArticle == null) {
+			return id + "번 게시물은 존재하지 않습니다.";
+		}
+
+		articleService.deleteArticle(foundArticle);
+
+		return id + "번 게시물을 삭제 했습니다.";
+	}
+
 }
