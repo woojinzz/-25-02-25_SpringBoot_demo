d3a27d1ee00f46f5a8a3534f50e23545816e5df2
woojinzz <dnwls3984@gmail.com>
2025-02-28 15:06:03 +0900

    resultData 생성 및 결과 값 보여주는 부분 수정

--- src/main/java/com/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/example/demo/controller/UsrMemberController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.example.demo.service.MemberService;
 import com.example.demo.util.Util;
 import com.example.demo.vo.Member;
+import com.example.demo.vo.ResultData;
 
 @Controller
 public class UsrMemberController {
@@ -18,40 +19,41 @@ public class UsrMemberController {
 
 	@GetMapping("/usr/member/doJoin")
 	@ResponseBody
-	public Object doJoin(String loginId, String loginPw, String loginPwChk, String name, String nickname, String cellphoneNum, String email) {
+	public ResultData doJoin(String loginId, String loginPw, String loginPwChk, String name, String nickname, String cellphoneNum, String email) {
 
 		if (Util.isEmpty(loginId))
-			return "아이디는 필수 입력 정보입니다.";
+			return ResultData.from("F-1", "아이디는 필수 입력 정보입니다.");
 
 		if (Util.isEmpty(loginPw))
-			return "비밀번호는 필수 입력 정보입니다.";
+			return ResultData.from("F-2", "비밀번호는 필수 입력 정보입니다.");
 
 		if (!loginPw.equals(loginPwChk))
-			return "비밀번호가 다릅니다.";
+			return ResultData.from("F-3", "비밀번호가 다릅니다.");
 
 		if (Util.isEmpty(name))
-			return "이름은 필수 입력 정보입니다.";
-
+			return ResultData.from("F-4", "이름은 필수 입력 정보입니다.");
+			
 		if (Util.isEmpty(nickname))
-			return "닉네임은 필수 입력 정보입니다.";
+			return ResultData.from("F-5", "닉네임은 필수 입력 정보입니다.");
 
 		if (Util.isEmpty(cellphoneNum))
-			return "번호는 필수 입력 정보입니다.";
+			return ResultData.from("F-6", "번호는 필수 입력 정보입니다.");
 
 		if (Util.isEmpty(email))
-			return "이메일은 필수 입력 정보입니다.";
+			return ResultData.from("F-7", "이메일은 필수 입력 정보입니다.");
 		
 		Member member = memberService.getMemberByLoginId(loginId);
 		
 		if (member != null) {
-			return loginId + "는 이미 가입된 아이디입니다.";
+			return ResultData.from("F-8", String.format("%s 는 이미 가입된 아이디입니다.", loginId));
 		}
 		
 		memberService.joinMember(loginId, loginPw, loginPwChk, name, nickname, cellphoneNum, email);
 
 		int id = memberService.getLastInsertId();
+		
+		return ResultData.from("S-1", String.format("%s 님이 가입되었습니다", loginId ), memberService.getMemberById(id));
 
-		return memberService.getMemberById(id);
 
 	}
 }
