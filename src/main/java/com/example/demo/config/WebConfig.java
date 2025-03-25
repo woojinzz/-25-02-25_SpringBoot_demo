a999254f7cf16428016f64dd346d814afa1f08e2
woojin <dnwls3984@gmail.com>
2025-03-26 00:10:31 +0900

    25-03-26 로그인, 로그아웃 인터셉터 진행증

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -5,18 +5,27 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.example.demo.interceptor.BeforActionInterceptor;
+import com.example.demo.interceptor.NeedLoginInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
 	private BeforActionInterceptor beforActionInterceptor;
+	private NeedLoginInterceptor needLoginInterceptor;
 
-	public WebConfig(BeforActionInterceptor beforActionInterceptor) {
+	public WebConfig(BeforActionInterceptor beforActionInterceptor, NeedLoginInterceptor needLoginInterceptor) {
 		this.beforActionInterceptor = beforActionInterceptor;
+		this.needLoginInterceptor = needLoginInterceptor;
 	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(beforActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**");
+
+		registry.addInterceptor(needLoginInterceptor).addPathPatterns("/usr/article/write")
+				.addPathPatterns("/usr/article/doWrite").addPathPatterns("/usr/article/modify")
+				.addPathPatterns("/usr/article/doModify").addPathPatterns("/usr/article/doDelete")
+				.addPathPatterns("/usr/member/doLogout");
+
 	}
 }
--- src/main/java/com/example/demo/interceptor/NeedLoginInterceptor.java
+++ src/main/java/com/example/demo/interceptor/NeedLoginInterceptor.java
@@ -3,19 +3,26 @@ package com.example.demo.interceptor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import com.example.demo.util.Util;
 import com.example.demo.vo.Rq;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class BeforActionInterceptor implements HandlerInterceptor {
+public class NeedLoginInterceptor implements HandlerInterceptor {
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-		Rq rq = new Rq(request);
-		request.setAttribute("rq", rq);
+		Rq rq = (Rq) request.getAttribute("rq");
+		
+		if (rq.getLoginedMemberId() == 0) {
+			response.setContentType("text/html; charset=UTF-8");
+			response.getWriter().append(Util.jsReplace("로그인 후 이용해주세요", "/"));
+			
+			return false; 
+		}
 
 		return HandlerInterceptor.super.preHandle(request, response, handler);
 	}
